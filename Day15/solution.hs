e700d7e5c8683b40664877d692f8244df3ed0521
Daniele Paolini <dan.floyd111@gmail.com>
2021-12-26 00:31:52 +0100

    Optimize part 1 and add part 2 solution

--- Day15/solution.hs
+++ Day15/solution.hs
@@ -1,4 +1,5 @@
 import Data.Char (digitToInt)
+import qualified Data.Set as Set
 import System.IO
 
 type Point = (Int, Int)
@@ -44,59 +45,81 @@ buildSquareMatrix baseValue n =
   let row = replicate n baseValue
    in replicate n row
 
-getLessRiskyUnvisitedPoint :: [[(Bool, Int)]] -> Point
-getLessRiskyUnvisitedPoint m =
-  let minValue = (maxBound :: Int)
-      n = length m
-      f minval minpoint (r, c)
-        | (r, c) == (n - 1, n - 1) =
-          if v < minval && not visited
-            then (r, c)
-            else minpoint
-        | c == n - 1 =
-          if v < minval && not visited
-            then f v (r, c) (r + 1, 0)
-            else f minval minpoint (r + 1, 0)
-        | otherwise =
-          if v < minval && not visited
-            then f v (r, c) (r, c + 1)
-            else f minval minpoint (r, c + 1)
-        where
-          visited = fst $ m !! r !! c
-          v = snd $ m !! r !! c
-   in f minValue (0, 0) (0, 0)
+updateRiskLevel ::
+  (Num a, Ord a) =>
+  [[a]] -> -- chitons
+  (Int, Int) -> -- point
+  [(Int, Int)] -> -- neighbors of the point
+  [[a]] -> -- risk matrix
+  Set.Set (a, (Int, Int)) -> -- set of (risk of the node, node) to be visited
+  ([[a]], Set.Set (a, (Int, Int))) -- updated risk matrix and set of nodes to be visited
+updateRiskLevel _ _ [] riskMatrix toBeVisited = (riskMatrix, toBeVisited)
+updateRiskLevel chitons (r, c) ((x, y) : ns) riskMatrix toBeVisited =
+  let alt = (riskMatrix !! r !! c) + (chitons !! x !! y)
+      currentRisk = riskMatrix !! x !! y
+      (riskMatrix', toBeVisited') =
+        if alt < currentRisk
+          then (replaceValueInSquareMatrix riskMatrix (x, y) alt, Set.insert (alt, (x, y)) toBeVisited)
+          else (riskMatrix, toBeVisited)
+   in updateRiskLevel chitons (r, c) ns riskMatrix' toBeVisited'
 
-updateDangerLevel [] chitonsGrid dangerGrid point = dangerGrid
-updateDangerLevel neighbors@((x, y) : ns) chitonsGrid dangerGrid (r, c) =
-  let alt = snd (dangerGrid !! r !! c) + (chitonsGrid !! x !! y)
-      (visited, currentDanger) = dangerGrid !! x !! y
-      dangerGrid' =
-        if alt < currentDanger
-          then replaceValueInSquareMatrix dangerGrid (x, y) (visited, alt)
-          else dangerGrid
-   in updateDangerLevel ns chitonsGrid dangerGrid' (r, c)
-
-searchLessDangerousPath :: [[Int]] -> [[(Bool, Int)]] -> (Int, Int) -> Int
-searchLessDangerousPath chitonsGrid dangerGrid (r, c)
-  | (r, c) == (n -1, n -1) = snd (dangerGrid !! r !! c)
-  | otherwise =
-    let dangerVal = snd $ dangerGrid !! r !! c
-        dangerGrid' = replaceValueInSquareMatrix dangerGrid (r, c) (True, dangerVal) -- mark visited
-        -- get unvisited neighbors
-        neighbors = filter (\(x, y) -> not (fst $ dangerGrid' !! x !! y)) (getNeighbors chitonsGrid (r, c))
-        dangerGrid'' = updateDangerLevel neighbors chitonsGrid dangerGrid' (r, c)
-        nextPoint = getLessRiskyUnvisitedPoint dangerGrid''
-     in searchLessDangerousPath chitonsGrid dangerGrid'' nextPoint
+-- chitons matrix, risk matrix, set of (risk, point) to be visited, end point -> danger of less risky path
+searchLessRiskyPath :: [[Int]] -> [[Int]] -> Set.Set (Int, Point) -> Point -> Int
+searchLessRiskyPath chitons riskMatrix toBeVisited endPoint =
+  if targetPoint == endPoint
+    then targetRisk
+    else searchLessRiskyPath chitons riskMatrix' toBeVisited'' endPoint
   where
-    n = length dangerGrid
+    (targetRisk, targetPoint) = Set.findMin toBeVisited
+    toBeVisited' = Set.deleteMin toBeVisited
+    neighbors = getNeighbors chitons targetPoint
+    (riskMatrix', toBeVisited'') = updateRiskLevel chitons targetPoint neighbors riskMatrix toBeVisited'
+
+-- Second part functions
+increaseRisk :: Int -> Int
+increaseRisk n = if n == 9 then 1 else n + 1
+
+increaseRow :: [Int] -> [Int]
+increaseRow = map increaseRisk
+
+deployRow :: Int -> [Int] -> [Int] -> [Int]
+deployRow 1 _ acc = acc
+deployRow n row acc =
+  let row' = increaseRow row
+   in deployRow (n -1) row' (acc ++ row')
+
+buildFullRow :: [Int] -> [Int]
+buildFullRow row = deployRow 5 row row
+
+buildSubMatricesList 1 _ acc = acc
+buildSubMatricesList n m acc =
+  let m' = map increaseRow m
+   in buildSubMatricesList (n -1) m' (acc ++ m')
+
+buildFullMatrix chitons =
+  let partial = buildSubMatricesList 5 chitons chitons
+   in map buildFullRow partial
+
+-- Using Djikstra algorithm to find the less dangerous path across the matrix
+-- A risk :: [[Int]] will store risk level for node (x,y) at position row: x , column: y of risk -> risk !! x !! y
+-- risk :: [[Int]] will optimize increase and decrease operations on risk values
+-- A Set of (risk, Point) will be used as a priority queue to get the less dangerous node to visit, a set is ordered
+-- so finding the minimum is trivial and speedy
 
 main = do
   content <- readFile "input.txt"
   let chitons = map toIntList (lines content)
-  -- specs document says that input is a n*n grid so it's safe to read just a "dimension", number of rows in this case
+  -- specs document says that input is a n*n grid so it's safe to read just a "dimension": the number of rows
   let n = length chitons
-  -- danger matrix tracks both risk level and if the position has been visited
-  let danger = replaceValueInSquareMatrix (buildSquareMatrix (False, maxBound :: Int) n) (0, 0) (False, 0)
+  let riskMatrix = replaceValueInSquareMatrix (buildSquareMatrix (maxBound :: Int) n) (0, 0) 0
 
-  let minimumDanger = searchLessDangerousPath chitons danger (0, 0)
-  putStrLn $ "Less dangerous path has a risk level of: " ++ show minimumDanger
+  let minimumDanger = searchLessRiskyPath chitons riskMatrix (Set.fromList [(0, (0, 0))]) (n -1, n -1)
+  putStrLn $ "First part: less dangerous path has a risk level of " ++ show minimumDanger
+
+  -- second part
+  let fullChitons = buildFullMatrix chitons
+  let w = length fullChitons
+  let fullRiskMatrix = replaceValueInSquareMatrix (buildSquareMatrix (maxBound :: Int) w) (0, 0) 0
+  let fullMinimumDanger = searchLessRiskyPath fullChitons fullRiskMatrix (Set.fromList [(0, (0, 0))]) (w -1, w -1)
+
+  putStrLn $ "Second part: less dangerous path has a risk level of " ++ show fullMinimumDanger
