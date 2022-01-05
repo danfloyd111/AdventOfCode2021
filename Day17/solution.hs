5a1600310539feac5c6099f1317b24915f56f5b1
Daniele Paolini <dan.floyd111@gmail.com>
2022-01-05 21:06:27 +0100

    Add solution for part 1

--- Day17/solution.hs
+++ Day17/solution.hs
@@ -1,5 +1,34 @@
 import System.IO
 
+readInt :: String -> Int
+readInt = read
+
+wordsWhen :: (Char -> Bool) -> String -> [String]
+wordsWhen p s = case dropWhile p s of
+  "" -> []
+  s' -> w : wordsWhen p s''
+    where
+      (w, s'') = break p s'
+
+-- takes a string in the format "x=A..B" or "y=A..B" where A and B are integers and returns (A,B)
+parseStartAndEndCoords :: String -> (Int, Int)
+parseStartAndEndCoords rawSpan =
+  let span = wordsWhen (== '.') (wordsWhen (== '=') rawSpan !! 1)
+      start = readInt $ head span
+      end = readInt $ last span
+   in (start, end)
+
+-- calculates the sum of the first n integers
+gaussSum :: Int -> Int
+gaussSum n = (n * (n + 1)) `div` 2
+
 main = do
-  inputLine <- readFile "input-sample.txt"
-  putStrLn "Hello tricky shot!"
+  inputLine <- readFile "input.txt"
+
+  let inputTokens = wordsWhen (== ',') inputLine
+      (xTargetMin, xTargetMax) = parseStartAndEndCoords (words (head inputTokens) !! 2)
+      (yTargetMin, yTargetMax) = parseStartAndEndCoords (last inputTokens)
+
+  let maxHeight = gaussSum (- yTargetMin -1)
+
+  putStrLn $ "Maximum height: " ++ show maxHeight
